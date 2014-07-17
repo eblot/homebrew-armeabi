b6353c80119599fe682308135bdb4434fee09433
Emmanuel Blot <emmanuel.blot@free.fr>
2014-07-17 17:20:26 +0200

    Upgrade ARM-EABI toolchain to GCC 4.9.1 and Newlib 2.1

--- arm-eabi-gcc49.rb
+++ arm-eabi-gcc49.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class ArmEabiGcc49 <Formula
-  url       'http://ftpmirror.gnu.org/gcc/gcc-4.9.0/gcc-4.9.0.tar.bz2'
+  url       'http://ftpmirror.gnu.org/gcc/gcc-4.9.1/gcc-4.9.1.tar.bz2'
   homepage  'http://gcc.gnu.org/'
-  sha1      'fbde8eb49f2b9e6961a870887cf7337d31cd4917'
+  sha1      '3f303f403053f0ce79530dae832811ecef91197e'
 
   keg_only 'Enable installation of several GCC versions'
 
@@ -13,16 +13,13 @@ class ArmEabiGcc49 <Formula
   depends_on 'cloog'
   depends_on 'isl'
   depends_on 'arm-eabi-binutils224'
-  depends_on 'gcc48' => :build
+  depends_on 'gcc49' => :build
 
-  resource "newlib20" do
-    url       'ftp://sourceware.org/pub/newlib/newlib-2.0.0.tar.gz'
-    sha1      'ea6b5727162453284791869e905f39fb8fab8d3f'
+  resource "newlib21" do
+    url       'ftp://sourceware.org/pub/newlib/newlib-2.1.0.tar.gz'
+    sha1      '364d569771866bf55cdbd1f8c4a6fa5c9cf2ef6c'
   end
 
-  # Issue in libgloss build with newlib 2.1
-  #  url       'ftp://sourceware.org/pub/newlib/newlib-2.1.0.tar.gz'
-  #  sha1      '364d569771866bf55cdbd1f8c4a6fa5c9cf2ef6c'
 
   def install
 
@@ -30,7 +27,7 @@ class ArmEabiGcc49 <Formula
 
     coredir = Dir.pwd
 
-    resource("newlib20").stage do
+    resource("newlib21").stage do
       system "ditto", Dir.pwd+'/libgloss', coredir+'/libgloss'
       system "ditto", Dir.pwd+'/newlib', coredir+'/newlib'
     end
@@ -42,11 +39,11 @@ class ArmEabiGcc49 <Formula
     isl = Formula.factory 'isl'
     libelf = Formula.factory 'libelf'
     binutils = Formula.factory armeabi
-    gcc48 = Formula.factory 'gcc48'
+    gcc49 = Formula.factory 'gcc49'
 
     # Fix up CFLAGS for cross compilation (default switches cause build issues)
-    ENV['CC'] = "#{gcc48.opt_prefix}/bin/gcc-4.8"
-    ENV['CXX'] = "#{gcc48.opt_prefix}/bin/g++-4.8"
+    ENV['CC'] = "#{gcc49.opt_prefix}/bin/gcc-4.9"
+    ENV['CXX'] = "#{gcc49.opt_prefix}/bin/g++-4.9"
     ENV['CFLAGS_FOR_BUILD'] = "-O2"
     ENV['CFLAGS'] = "-O2"
     ENV['CFLAGS_FOR_TARGET'] = "-O2"
@@ -57,7 +54,7 @@ class ArmEabiGcc49 <Formula
     build_dir='build'
     mkdir build_dir
     Dir.chdir build_dir do
-      system "../configure", "--prefix=#{prefix}", "--target=arm-eabi",
+      system coredir+"/configure", "--prefix=#{prefix}", "--target=arm-eabi",
                   "--disable-shared", "--with-gnu-as", "--with-gnu-ld",
                   "--with-newlib", "--enable-softfloat", "--disable-bigendian",
                   "--disable-fpu", "--disable-underscore", "--enable-multilibs",
