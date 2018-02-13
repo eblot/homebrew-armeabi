b81ec77f9ed3222fc709b3981e05cb9895ef784e
Emmanuel Blot <eblot.com@gmail.com>
2018-02-13 22:55:37 +0100

    Upgrade to v2.30

--- arm-none-eabi-binutils.rb
+++ arm-none-eabi-binutils.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class ArmNoneEabiBinutils <Formula
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.29.1.tar.bz2'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.xz'
   homepage 'http://www.gnu.org/software/binutils/'
-  sha256 '1509dff41369fb70aed23682351b663b56db894034773e6dbf7d5d6071fc55cc'
+  sha256 '6e46b8aeae2f727a36f0bd9505e405768a72218f1796f0d09757d45209871ae6'
 
   depends_on 'gmp'
   depends_on 'mpfr'
