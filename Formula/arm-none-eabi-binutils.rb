f8b706e5be831ded880e1db58737fd3b49043d22
Emmanuel Blot <emmanuel.blot@sifive.com>
2021-04-20 13:52:13 +0200

    binutils: update to 2.36.1
    use a subdir for building and rename info file to avoid conflict w/
    native toolchain (no more issue to link the formula)

--- Formula/arm-none-eabi-binutils.rb
+++ Formula/arm-none-eabi-binutils.rb
@@ -3,21 +3,26 @@ require "formula"
 class ArmNoneEabiBinutils < Formula
   desc "GNU Binutils for OS-less ARM 32-bit architecture"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz"
-  sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz"
+  sha256 "e81d9edf373f193af428a0f256674aea62a9d74dfe93f65192d4eae030b0f3b0"
 
   depends_on "gmp"
   depends_on "mpfr"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--target=arm-none-eabi",
-                "--disable-shared", "--disable-nls",
-                "--with-gmp=#{Formulary.factory("gmp").prefix}",
-                "--with-mpfr=#{Formulary.factory("mpfr").prefix}",
-                "--disable-cloog-version-check",
-                "--enable-multilibs", "--enable-interwork", "--enable-lto",
-                "--disable-werror", "--disable-debug"
-    system "make"
-    system "make install"
+    mkdir "build" do
+      system "../configure", "--target=arm-none-eabi-",
+                  "--prefix=#{prefix}", "--disable-shared", "--disable-nls",
+                  "--with-gmp=#{Formulary.factory("gmp").prefix}",
+                  "--with-mpfr=#{Formulary.factory("mpfr").prefix}",
+                  "--disable-cloog-version-check",
+                  "--enable-multilibs", "--enable-interwork", "--enable-lto",
+                  "--disable-werror", "--disable-debug"
+      system "make"
+      system "make install"
+      system "(cd #{prefix}/share/info && \
+               for info in *.info; do \
+                  mv $info $(echo $info | sed 's/^/arm-none-eabi-/'); done)"
+    end
   end
 end
