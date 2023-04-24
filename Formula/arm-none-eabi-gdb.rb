d4f82493930d123b24b837d2d788a6cc82d761cc
Emmanuel Blot <emmanuel.blot@free.fr>
2023-04-24 15:33:39 +0200

    gdb: update to 13.1

--- Formula/arm-none-eabi-gdb.rb
+++ Formula/arm-none-eabi-gdb.rb
@@ -1,69 +1,61 @@
 require "formula"
 
 class ArmNoneEabiGdb < Formula
-  desc "GNU debugger for ARM 32-bit architecture"
-  homepage "https://www.gnu.org/software/gdb/"
-  url "http://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz"
-  sha256 "f82f1eceeec14a3afa2de8d9b0d3c91d5a3820e23e0a01bbb70ef9f0276b62c0"
+  homepage "https://www.gnu.org/software/binutils/"
+  desc "GNU debugger for bare metal ARM 32-bit targets"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-13.1.tar.xz"
+  sha256 "115ad5c18d69a6be2ab15882d365dda2a2211c14f480b3502c6eba576e2e95a0"
 
   depends_on "gmp"
   depends_on "libmpc"
   depends_on "mpfr"
   depends_on "readline"
   depends_on "expat"
-  depends_on "python3"
-  depends_on "arm-none-eabi-binutils"
+  depends_on "python"
+  depends_on "texinfo" => :build
+  depends_on "flex" => :build
+  depends_on "bison" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   # Linux dependencies.
   depends_on "guile" unless OS.mac?
 
-  # need to regenerate configure script after applying patch
-  if OS.mac?
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-
-    patch :DATA if OS.mac?
-  end
-
   def install
-    if OS.mac?
-      # as dirty as it sounds, I'm tired of autotools...
-      autoconf = Formulary.factory "autoconf"
-      inreplace "config/override.m4", "[2.69]", "[#{autoconf.version}]"
-      system "autoreconf gdb 2>/dev/null"
-    end
     mkdir "build" do
-      system "../configure", "--prefix=#{prefix}",
-                "--target=arm-none-eabi",
-                "--with-gmp=#{Formulary.factory("gmp").prefix}",
-                "--with-mpfr=#{Formulary.factory("mpfr").prefix}",
-                "--with-mpc=#{Formulary.factory("libmpc").prefix}",
-                "--with-readline=#{Formulary.factory("readline").prefix}",
-                "--with-python=#{Formulary.factory("python3").bin}/python3",
-                "--with-expat=#{Formulary.factory("expat").prefix}",
-                "--without-cloog",
-                "--enable-lto", "--disable-werror"
+      system "../configure",
+             "--prefix=#{prefix}",
+             "--target=arm-none-eabi",
+             "--disable-nls",
+             "--with-gmp=#{Formulary.factory("gmp").prefix}",
+             "--with-mpfr=#{Formulary.factory("mpfr").prefix}",
+             "--with-mpc=#{Formulary.factory("libmpc").prefix}",
+             "--with-readline=#{Formulary.factory("readline").prefix}",
+             "--with-python3=#{Formulary.factory("python").prefix}/bin/python3",
+             "--with-expat=#{Formulary.factory("expat").prefix}",
+             "--without-cloog",
+             "--enable-multilibs",
+             "--enable-lto",
+             "--enable-gdb",
+             "--disable-binutils",
+             "--disable-ld",
+             "--disable-gold",
+             "--disable-gas",
+             "--disable-sim",
+             "--disable-gprof",
+             "--disable-werror",
+             "--disable-debug",
+             "--disable-ld",
+             "--disable-gold",
+             "--disable-gas",
+             "--disable-sim",
+             "--disable-gprof",
+             "--disable-gold"
       system "make"
       system "make install"
       system "(cd #{prefix}/share/info && \
                for info in *.info; do \
-                  mv $info $(echo $info | sed 's/^/arm-none-eabi-/'); done &&
-               rm -f arm-none-eabi-bfd.info; true)"
-      # remove bfd.info to avoid conflict with the binutils twin.
+                  mv $info $(echo $info | sed 's/^/arm-none-eabi-/'); done)"
     end
   end
 end
-
-# This patch addresses a configuration issue where GDB does not include
-# string.h, which breaks build as strncmp is no longer declared on macOS 11
-__END__
---- a/gdb/acinclude.m4  2020-11-26 10:10:52.000000000 +0100
-+++ b/gdb/acinclude.m4  2020-11-26 10:12:25.000000000 +0100
-@@ -362,6 +362,7 @@
-   AC_CACHE_CHECK([$1], [$2],
-   [AC_TRY_LINK(
-   [#include <stdlib.h>
-+  #include <string.h>
-   #include "bfd.h"
-   #include "$4"
-   ],
